a60e44cf3e6d9d4a93bda3946ddbb644a8eb1981
OCP <oscarcarballopuebla@gmail.com>
2026-03-12 01:22:50 +0100

    feat: add solutions for cses problems 1619 and 1629

---
+++ cses/1619.rs
@@ -0,0 +1,86 @@
+#![allow(unused_imports)]
+use std::cmp::{max, min};
+use std::collections::{BTreeMap, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+#[allow(unused)]
+const MOD: usize = 1_000_000_007;
+
+struct Scanner<R> {
+    reader: R,
+    tokens: Vec<String>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            tokens: Vec::new(),
+        }
+    }
+
+    fn next<T: FromStr>(&mut self) -> T {
+        while self.tokens.is_empty() {
+            let mut line = String::new();
+            self.reader
+                .read_line(&mut line)
+                .expect("Failed to read line");
+            self.tokens = line
+                .split_whitespace()
+                .rev()
+                .map(|s| s.to_string())
+                .collect();
+        }
+        self.tokens
+            .pop()
+            .expect("No more tokens")
+            .parse()
+            .ok()
+            .expect("Failed to parse")
+    }
+}
+
+#[allow(unused)]
+fn pow_mod(mut base: usize, mut exp: usize) -> usize {
+    let mut res = 1;
+    base %= MOD;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            res = (res * base) % MOD;
+        }
+        exp /= 2;
+        base = (base * base) % MOD;
+    }
+    res
+}
+
+#[allow(unused)]
+fn mex(set: &HashSet<usize>) -> usize {
+    (0..).find(|i| !set.contains(i)).unwrap()
+}
+
+fn solve(sc: &mut Scanner<io::StdinLock>) {
+    let t: usize = sc.next();
+    let mut events: Vec<(usize, isize)> = Vec::new();
+
+    for _ in 0..t {
+        let (a, b): (usize, usize) = (sc.next(), sc.next());
+        events.push((a, 1));
+        events.push((b, -1));
+    }
+    events.sort();
+    let (mut act, mut sol) = (0, 0);
+    for (_, p) in events {
+        act += p;
+        sol = max(sol, act);
+    }
+    println!("{}", sol);
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut scanner = Scanner::new(stdin.lock());
+
+    solve(&mut scanner);
+}
